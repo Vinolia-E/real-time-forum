d9c85b19991f6ab397ca5aec1e045f259d003d86
vandisi <vinolia.b.19@gmail.com>
2025-05-29 15:37:40 +0300

    fear: Call function that initialize database in main function

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"real_time_forum/backend/sqlite"
+)
+
+func main() {
+	err := sqlite.InitDatabase("real_time_database.db")
+	if err != nil {
+		fmt.Println("Error initializing database:", err)
+		return
+	}
+}
