c02c5cf961667526c1b2b724ead922896a1ac511
vandisi <vinolia.b.19@gmail.com>
2025-06-15 17:36:43 +0300

    fix: Properly handle register js function

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,15 @@
-fetch("http://localhost:8080/register", {
+const form = document.getElementById("registerForm");
+form.addEventListener("submit", function(e) {
+   e.preventDefault();
+   fetch("http://localhost:8080/register", {
     method: "POST",
     body: new FormData(form),
-})
-.then(response => response.json())
-.then(data => {
-    console.log("Success:", data.message); // or display it in the UI
-})
-.catch(error => {
+   })
+   .then(response => response.json())
+   .then(data => {
+    console.log("Success:", data.message); 
+   })
+   .catch((error) => {
     console.error("Error:", error);
-});
+    });
+}); 
